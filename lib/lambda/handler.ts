2c971b621c1f2df6e6236fc7cbdab59eb780cf68
Anson Kee <akee@legalzoom.com>
2023-12-19 16:30:18 -0800

    fix console log env

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,5 +1,5 @@
 export async function handler(event: string, context: string) {
-  console.log("Stage name is: " + process.env.stage);
+  console.log("Stage name is: " + process.env.stageName);
   return {
     body: "Hello Lambda Function",
     statusCode: 200,
